9211c3ca3e8f607b224de45bc5b10d0f344e9cfc
rafaelcnagy <rafaelnagy@hotmail.com>
2022-10-28 23:42:58 -0300

    Create models

--- uploader/models.py
+++ uploader/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+    balance = models.IntegerField(default=0)
+
+class Product(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    producers = models.ManyToManyField(Person, related_name='producers')
+    affiliates = models.ManyToManyField(Person, related_name='affiliate')
+
+class Transaction(models.Model):
+    type = models.IntegerField()
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    value = models.IntegerField()
