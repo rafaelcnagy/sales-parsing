987120d27fe9eb434b5e5b9ef4041f7632ae202d
rafaelcnagy <rafaelnagy@hotmail.com>
2022-10-29 23:46:12 -0300

    Add file parser

---
+++ uploader/parser.py
@@ -0,0 +1,45 @@
+import datetime
+from uploader.models import Person, Product, Transaction
+from textwrap import wrap
+
+
+def parse_sales(file):
+    def detach_transaction(line):
+        return int(line[:1]), line[1:26], line[26:56].strip(), int(line[56:66]), line[66:].strip()
+    
+    file_content = file.read()
+    
+    for content_line in wrap(file_content.decode('utf-8'), 86):
+        type, date_str, product_name, value, person_name = detach_transaction(content_line)
+        print(type, date_str, product_name, value, person_name)
+
+        date = datetime.datetime.strptime(date_str, '%Y-%m-%dT%H:%M:%S%z')
+        
+        product, _ = Product.objects.get_or_create(name=product_name)
+        person, _ = Person.objects.get_or_create(name=person_name)
+       
+        transaction = Transaction.objects.create(
+            type=type,
+            date=date,
+            product=product,
+            value=value,
+            person=person
+        )
+
+        if type == 1:
+            product.producers.add(person)
+            person.balance += value
+            product.save()
+        elif type == 2:
+            product.affiliates.add(person)
+            person.balance += value
+            product.save()
+        elif type == 3:
+            person.balance -= value
+        elif type == 4:
+            person.balance += value
+        else:
+            raise NotImplementedError()
+
+        person.save()
+
--- uploader/views.py
+++ uploader/views.py
@@ -1,8 +1,19 @@
+from django.shortcuts import HttpResponseRedirect
 from uploader.forms import UploadFileForm
 from django.views.generic.edit import FormView
+
+from uploader.parser import parse_sales
 
 
 class UploadFileView(FormView):
     template_name = 'uploader/form.html'
     form_class = UploadFileForm
     success_url = '/'
+
+    def post(self, request, *args, **kwargs):
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+        if form.is_valid():
+            parse_sales(request.FILES['file'])
+            return HttpResponseRedirect(self.success_url)
+
