a18afe3d0a4aecdadf926cbc5d392e26a3ce09aa
rafaelcnagy <rafaelnagy@hotmail.com>
2022-11-01 00:00:40 -0300

    fix type

--- uploader/validators.py
+++ uploader/validators.py
@@ -1,16 +1,15 @@
 import os
 import re
 from django.core.exceptions import ValidationError
-from sqlalchemy import null
 
-def validate_file_extension(file) -> null:
+def validate_file_extension(file) -> None:
     ''' Check if file extension is .txt '''
     ext = os.path.splitext(file.name)[1]  # [0] returns path+filename
     valid_extensions = ['.txt']
     if not ext.lower() in valid_extensions:
         raise ValidationError('Unsupported file extension.')
 
-def validate_data_format(file) -> null:
+def validate_data_format(file) -> None:
     ''' Check data format matchs with pattern '''
     pattern = re.compile('^\d\d{4}\-\d{2}\-\d{2}T\d{2}:\d{2}:\d{2}\-\d{2}:\d{2}.{30}\d{10}.+$')
     for line in file:
